5c24dd23f20ad9434fbf0b4ad5ad02be5a021eca
poixao <1412634436@qq.com>
2022-06-02 20:41:59 +0800

    hot-fix commit

--- src/main/java/com/study/git/hello.java
+++ src/main/java/com/study/git/hello.java
@@ -8,5 +8,6 @@ public class hello {
         System.out.println("hello");
         System.out.println("hello 2");
         System.out.println("hello 3");
+        System.out.println("hello 4");
     }
 }
