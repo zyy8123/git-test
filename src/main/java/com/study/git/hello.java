65af7da2c672ec8548242358b67907def5f3f7cf
poixao <1412634436@qq.com>
2022-06-02 17:21:08 +0800

    third commit

--- src/main/java/com/study/git/hello.java
+++ src/main/java/com/study/git/hello.java
@@ -7,5 +7,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello 2");
+        System.out.println("hello 3");
     }
 }
