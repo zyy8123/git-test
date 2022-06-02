f02b6d3232e4b55d9660b47e2dc17bbf1cd49c9a
poixao <74133231+zyy8123@users.noreply.github.com>
2022-06-02 23:26:48 +0800

    Update hello.java

--- src/main/java/com/study/git/hello.java
+++ src/main/java/com/study/git/hello.java
@@ -11,5 +11,6 @@ public class hello {
         System.out.println("hello 4");
         System.out.println("master test!");
         System.out.println("pull test!");      
+        System.out.println("gitee test!");         
     }
 }
